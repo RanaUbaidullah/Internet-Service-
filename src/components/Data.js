2791cc5a07c09dfb2a314ac930053d14a0a5f944
Heart <researchfigma@gmail.com>
2023-06-17 23:51:29 +0500

    update data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -15,12 +15,16 @@ export const Data = () => {
   console.log(singleProductData);
 
   if (isLoading) return <h1> Loading...</h1>;
-  return <div> Data: 
+  return <div>
+   <h1 style={{position:"fixed" , left: "50%"}}>Data</h1> 
     {allProductsData.products.map((data)=>(
       <>
+      <div style={{width: "400px", border: "solid 5px red",margin:"30px" , alignItems:"center"}}>
+      <h1>Id: {data.id}</h1>
       <h1>Title: {data.title}</h1>
       <h2>Description: {data.description}</h2>
       <h5>Category: {data.category}</h5>
+      </div>
       </>
     ))}
   </div>;
