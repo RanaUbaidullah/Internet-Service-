62d6e07a99f673afac1f3b246d1fa25db86ce23e
Heart <researchfigma@gmail.com>
2023-06-18 11:20:25 +0500

    Basic componet add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import { Data } from "./components/Data";
+// import { Data } from "./components/Data";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { productsApi } from "./features/apiSlice";
-
+import Home from "./components/Home/Home";
 function App() {
   return (
     <Provider store={store}>
       <ApiProvider api={productsApi}>
         <div className="App">
-          <Data />
+          <Home />
         </div>
       </ApiProvider>
     </Provider>
